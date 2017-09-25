cf644e00df25f543f41349a56ee30287c5cb56a7
etruong1231 <etruong7292@gmail.com>
2017-09-25 16:12:38 -0700

    Add files via upload

---
+++ out/CMakeFiles/othelloaistudent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/othelloaistudent.dir/core/MyOthelloAI.cpp.o"
+  "CMakeFiles/othelloaistudent.dir/core/MyOthelloAI2.cpp.o"
+  "lib/libothelloaistudent.pdb"
+  "lib/libothelloaistudent.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/othelloaistudent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
